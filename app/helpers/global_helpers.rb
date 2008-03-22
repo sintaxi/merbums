6916d28d041e6a9119f5197b5aef4cbeecb89688
Shay Arnett <shayarnett@gmail.com>
2008-03-22 18:50:09 -0400

    fixed can_edit?

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,8 +1,8 @@
 module Merb
   module GlobalHelpers
     # helpers defined here available to all views.
-    def can_edit?
-      @post.user_id == current_user.id || is_admin?
+    def can_edit?(post)
+      post.user_id == current_user.id || is_admin?
     end
   end
 end
