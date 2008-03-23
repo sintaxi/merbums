028728024b6a8746dc6016ad135d0cd6d411c3d1
Shay Arnett <shayarnett@gmail.com>
2008-03-22 20:03:36 -0400

    duh

--- schema/migrations/008_add_sticky_to_topics.rb
+++
@@ -1,9 +0,0 @@
-class AddStickyToTopics < ActiveRecord::Migration
-  def self.up
-    add_column :topics, :sticky, :boolean, :default => false
-  end
-  
-  def self.down
-    remove_column :topics, :sticky
-  end
-end
